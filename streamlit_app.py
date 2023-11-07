44f878d84b1862d2106319078f0bb692c7edc39a
Sujith Kumar <100142128+sujithkumar2003@users.noreply.github.com>
2023-11-07 13:51:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ with st.sidebar:
     st.title('🤗💬 HugChat')
     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
         st.success('HuggingFace Login credentials already provided!', icon='✅')
+        hf_email = st.secrets['EMAIL']
+        hf_pass = st.secrets['PASS']
+    else:
         hf_email = "krsujithkumar1994@gmail.com"
         hf_pass = "Rushi@2003"
-    else:
-        hf_email = st.text_input('Enter E-mail:', type='password')
-        hf_pass = st.text_input('Enter password:', type='password')
         if not (hf_email and hf_pass):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
